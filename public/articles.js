40ef5008dc0abee40be933d92e4835d75f819971
alekh-ai <78842369+alekh-ai@users.noreply.github.com>
2021-02-15 15:36:17 +0530

    blog added

---
+++ public/articles.js
@@ -0,0 +1,33 @@
+articles = [
+	{
+		title: "Interrogating and Shaping the World Through Science",
+		author: "Ainissa Ramirez",
+		content: `What I noticed over the years is that people were starting to see science as entertainment and not as a tool or a lens to understand the world. The thing that scientists do is ask great questions. We need people who can interrogate and probe the world so they can develop their muscle of being critical thinkers. I saw that missing. I thought that I was becoming part of the problem of just showing science's entertainment with great demonstrations. I was hooking them, but I wasn't doing the next step, which is to say, “This is the enterprise of science; it's a great way to understand the world, and with it you can shape the world.” `,
+	},
+	{
+		title: `Computation All the Way Down`,
+		author: "Stephen Wolfram",
+		content: `We're now in this situation where people just assume that science can compute everything, that if we have all the right input data and we have the right models, science will figure it out. If we learn that our universe is fundamentally computational, that throws us right into the idea that computation is a paradigm you have to care about. The big transition was from using equations to describe how everything works to using programs and computation to describe how things work. And that's a transition that has happened after 300 years of equations. The transition time to using programs has been remarkably quick, a decade or two. One area that was a holdout, despite the transition of many fields of science into the computational models direction, was fundamental physics.`,
+	},
+	{
+		title: `A Very Bumpy Ride`,
+		author: "Larry Brilliant, MD",
+		content: `We need to have a strong WHO, a strong United Nations, a strong global alliance for vaccines and immunizations (GAVI), a strong Global Fund, and all these different organelles that make it possible for us to deal with global threats. I would extend it a little bit out of my lane to say we need desperately to deal with climate change, nuclear proliferation, drought, and famine. But in the area that I know, we can't stop a pandemic without having global collaboration. We have failed to learn the lessons of Taiwan, Vietnam, Singapore, Korea, New Zealand, Iceland—the countries that have done really well—because we don't have a strong way to take the best lessons from the success stories in dealing with this pandemic and globalizing them. This is because we deal with disinformation, and because we hold up the Swedish example even though it wasn't a good example of how to deal with the pandemic, and because we don't have a love of science in the leadership of the world, and because we don't talk to each other in the way that we need to.`,
+	},
+	{
+		title: `Shame Can Lead to Real Change Right Now`,
+		author: "Jennifer Jacquet ",
+		content: `After the storm on the Capitol, ex-President Barack Obama spoke of a "moment of great shame and embarrassment for our country". Such confessions are not uncommon in the USA: after Donald Trump's election in 2016, many Americans admitted that they were ashamed. And when Trump refused to acknowledge his electoral defeat in November, winner Joe Biden spoke of "an embarrassment". What is it about shame in the face of one's own country? American Jennifer Jacquet, a professor at NYU, has been researching punishment, guilt and shame for years.`,
+	},
+	{
+		title: `Mary Catherine Bateson: Systems Thinker`,
+		author: "Mary Catherine Bateson ",
+		content: `From the early days of Edge, Catherine Bateson was the gift that kept giving. Beginning in 1998, with her response to “What Questions Are You Asking Yourself?” through “The Last Question” in 2018, she exemplified the role of the Third Culture intellectual: “those scientists and other thinkers in the empirical world who, through their work and expository writing, are taking the place of the traditional intellectual in rendering visible the deeper meanings of our lives, redefining who and what we are.”`,
+	},
+
+	// {
+	// 	title: ``,
+	// 	author: "",
+	// 	content: ``,
+	// },
+];
